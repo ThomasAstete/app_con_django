882947b745e43290ebd454fac88360bd5fc84a50
Thomas Astete <thomas.astete@inacapmail.cl>
2025-09-01 14:40:14 -0400

    .
    cambios en view, urls y creacion de html de lista

--- bienvenida/bienvenida/view.py
+++ bienvenida/bienvenida/view.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Producto
 
-def inicio(request):
- return HttpResponse("Hola mundo desde Django")
+def lista_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'productos/lista.html', {'productos': productos})
